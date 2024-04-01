c5296c216240765297ee2418231a3f929b330aec
Coltarm <colin.treve@laposte.net>
2024-04-01 21:05:28 +0200

    petite correction bug et restructuration du code

--- PeersimForDHT/src/DHT/DHTNode.java
+++ PeersimForDHT/src/DHT/DHTNode.java
@@ -16,12 +16,18 @@ import java.util.logging.Logger;
 import java.util.logging.FileHandler;
 
 public class DHTNode implements EDProtocol{
+	/*
+	 * On déclare les différent Logger
+	 */
 
 	private static final Logger Logger_message= Logger.getLogger(DHTNode.class.getName());
 	private static final Logger Logger_ajout_node= Logger.getLogger(DHTNode.class.getName());
-	private static final Logger Logger_= Logger.getLogger(DHTNode.class.getName());
+	private static final Logger Logger_data= Logger.getLogger(DHTNode.class.getName());
 
-
+/*
+* On définit toutes les variable utilisé
+*
+* */
 	private int transportPid;
 	public int id_transp;
 	private boolean addingNode = false; // on utilise un booléen pour savoir si on est en train d'ajouter un noeuds et si c'est le cas
@@ -52,70 +58,67 @@ public class DHTNode implements EDProtocol{
 		this.mypid = Configuration.getPid(prefix + ".myself");
 		System.out.println("mypid = "+ this.mypid);
 		this.transport = null;
+		/*
+		 * Configuration des Logger
+		 */
 		try{
-			FileHandler fileHandler_message = new FileHandler("message");
+			FileHandler fileHandler_message = new FileHandler("message.log");
 			FileHandler fileHandler_join_node = new FileHandler("join_node.log");
+			FileHandler fileHandler_data = new FileHandler("data.log");
 			Logger_message.addHandler(fileHandler_message);
 			Logger_ajout_node.addHandler(fileHandler_join_node);
+			Logger_data.addHandler(fileHandler_data);
 		}
 		catch(IOException e){
 			Logger_ajout_node.severe("erreur");
 			Logger_message.severe("erreur");
 		}
 	}
-	
+
+	/*
+	* On définit toutes les méthodes pour configuré ou modifier les configurations de notre noeuds
+	* */
 	public void setIdList(ArrayList<Integer> list_id) {this.list_id = list_id;	}
 	
-	// on règle les voisins du noeuds
 	
 	
-	public void setTransportLayer(int id_transp, int nodeId) {
+	public void setTransportLayer(int id_transp, int nodeId) { // fonction pour configurer la couche transport et affecter l'ID du noeuds
 		this.ID = nodeId;
 		this.id_transp = id_transp;
 		this.transport = (HWTransport) Network.get(this.id_transp).getProtocol(this.transportPid);
 	    }
-	// on réalise deux fonction pour setter les voisin du noeuds
+	
+	/*
+	 * Configuration des voisins
+	 */
 	public void setRightNeighboor(DHTNode rightNei) {
 		this.rightNeighboor = rightNei;
-		//System.out.println("le noeuds "+ this.id_transp+" à comme voisin droit "+ this.rightNeighboor.id_transp);
+		System.out.println("le noeuds "+ this.getID()+" à comme voisin droit "+ this.rightNeighboor.getID());
 		
 	}
 	
 	public void setLeftNeighboor(DHTNode leftNei) {
 		this.leftNeighboor = leftNei;
-		//System.out.println("le noeuds"+ this.id_transp+" à comme voisin  "+ this.leftNeighboor.id_transp);
+		System.out.println("le noeuds"+ this.getID()+" à comme voisin gauche "+ this.leftNeighboor.getID());
 
 	}
-	
-    public void send(Message msg, Node dest) {
-    System.out.println("envoi message " +msg);
-	this.transport.send(getMyNode(), dest, msg, this.mypid);
-    }
-
-    //affichage a la reception
-    private void receive(Message msg ) {
-
-    	if (this.getID()>msg.getID()) {
-    		this.send(msg, this.rightNeighboor.getMyNode());
-    	}
-    	else if(this.getID()<msg.getID()) {
-    		this.send(msg,(Node) this.rightNeighboor.getMyNode());
-    	}
-    	else {
-		System.out.println(this + ": Received " + msg.getContent());
-    		int rand =random.nextInt(1,100);
-    		if (rand<=5) {
-       			this.send(msg, (Node) this.leftNeighboor);
-       		}
-       		else if(rand>=95) {
-       			this.send(msg, (Node) this.rightNeighboor);
-    		}
-			else if (rand<=15) {this.list_data.add(new Data());}
 
 
+/*
+méthodes pour définir le comportement à la réception d'un événement
+ */
+	public void processEvent( Node node, int pid, Object event ) {
+		try {Message msg = (Message) event;
+			this.root(msg);// à la reception on caste l'évenement comme un message et on l'envoi à la méthode root
 		}
-    }
-    private void root(Message msg){
+		catch(Exception e){System.out.println(("problem" + e));}
+
+	}
+
+	/*
+	*  méthode root sert à orienté les message en fonction de leurs nature
+	* */
+	private void root(Message msg){
 		if (!this.addingNode) {
 			switch (msg.getType()) { // on fait la différence entre les message et les noeuds à ajouter et la data
 				case "MSG":
@@ -125,12 +128,59 @@ public class DHTNode implements EDProtocol{
 					this.root_node(msg);
 					break;
 				case "DATA":
-					this.root_data(msg);
+					this.put_data(msg);
 					break;
 			}
 		}
 		else {messQueue.add(msg);}
 	}
+    public void send(Message msg, Node dest) {
+    //System.out.println("envoi message " +msg);
+	this.transport.send(getMyNode(), dest, msg, this.mypid);
+    }
+
+	/*Méthode qui traite les message dans la liste d'attente*/
+    /private void treat_waiting_mess() {// fonction qui redirige les message après qu'un noeuds est été ajouté
+		while(!this.messQueue.isEmpty()) {
+			root(this.messQueue.poll());
+		}
+	}
+
+	//affichage a la reception
+	/*
+	* Méthode receive réceptionne les messages de types messages
+	* soit les renvoie en direction du destinataire
+	* soit il est le destinataire et il peut renvoyer le message à un de ses voisin, il peut aussi créer une nouvelle data et demander à la placer dans la DHT
+	* */
+    private void receive(Message msg ) {
+		// on vérifie le destinataire et on renvoi le message si nécessaire
+    	if (this.getID()>msg.getID()) {
+    		this.send(msg, this.rightNeighboor.getMyNode());
+    	}
+    	else if(this.getID()<msg.getID()) {
+    		this.send(msg,(Node) this.rightNeighboor.getMyNode());
+    	}
+    	else {
+    	System.out.println("message pour moi");
+		Logger_data.info(" noeud "+ getID()+" reçoit message"  );
+		// on définit l'action qu'on effectue en fonction d'un nombre aléatoire
+    		int rand =random.nextInt(1,100);
+    		if (rand<=10) {
+       			this.send(msg, (Node) this.leftNeighboor);
+       		}
+       		else if(rand>=90) {
+       			this.send(msg, (Node) this.rightNeighboor);
+    		}
+			else if (rand<=25) {
+				Data data = new Data();
+				Logger_data.info("le noeuds : "+getID()+" a crée la donnée "+ data.getID());
+				this.put_data(new Message(data));}
+		}
+    }
+
+	/*
+	* Méthode pour orienté les noeuds, savoir si il faut le transmettre à un de ses voisin où si celui ci doit être ajouté comme voisin
+	* */
 	private void root_node(Message msg){
 		if (msg.getSubtype().equals("NODE_TO_ADD")){
 			join(msg.getNode());
@@ -138,55 +188,110 @@ public class DHTNode implements EDProtocol{
 		else if(msg.getContent().equals("LEFT")){
 			Logger_ajout_node.info("le node "+ getID() +" a ajouté"+ msg.getNode().getID()+" comme voisin gauche" );
 			this.addingNode=true;
-			setLeftNeighboor(msg.getNode());
+			this.setLeftNeighboor(msg.getNode());
 			this.addingNode=false;
 		}
 		else if(msg.getContent().equals("RIGHT")){
 			Logger_ajout_node.info("le node "+ getID() +" a pour voisin "+ msg.getNode().getID() );
 			this.addingNode = true;
-			setRightNeighboor(msg.getNode());
+			this.setRightNeighboor(msg.getNode());
 			this.addingNode=false;
 
 		}
 	}
-    
-    private void treat_waiting_mess() {// fonction qui redirige les message après qu'un noeuds est été ajouté
-    	while(!this.messQueue.isEmpty()) {
-    		root(this.messQueue.poll());
-    	}
-    } 
-    public void processEvent( Node node, int pid, Object event ) {
-		try {Message msg = (Message) event;
-			this.root(msg);
-		}
-		catch(Exception e){System.out.println(("problem"));}
+	
 
-        }
-	private void root_data(Message msg){
+	/*
+	* Méthode qui envoi vers la méthode get si on désir placer une donnée ou si on souhaite en récupérer
+	* */
+	private void root_data(Message msg) {
+		switch(msg.getSubtype()) {
+		case "PUT":
+			put_data(msg);
+			break;
+		case "GET":
+			get_data(msg);
+			break;
+		}
+	}
+    
+	private void put_data(Message msg){
 		Data data = msg.getData();
-		if(msg.getSubtype().equals("ADD_DATA")){
+		if(msg.getContent().equals("ADD")){ //si on est chargé de la stocké 
+			Logger_data.info("Le noeuds : "+ getID() +" prend la data "+ data.getID());
 			list_data.add(data);
 		}
 		else {
 			int distance = Math.abs(getID() - data.getID());
 			int distance_data_leftneig = Math.abs(leftNeighboor.getID() - data.getID());
 			int distance_data_rightneig = Math.abs(rightNeighboor.getID() - data.getID());
-			Message sendData = new Message(data);
+			Message sendData = new Message(data,"PUT","");
 			if (distance > distance_data_rightneig) {
 				this.send(sendData, rightNeighboor.getMyNode());
 			} else if (distance > distance_data_leftneig) {
 				this.send(sendData, leftNeighboor.getMyNode());
 			} else {
-				sendData = new Message(data, "test");
+				sendData = new Message(data, "PUT","ADD");
 				this.send(sendData, leftNeighboor.getMyNode());
 				this.send(sendData, rightNeighboor.getMyNode());
+				Logger_data.info("Le noeuds : "+ getID() +" prend la data "+ data.getID());
 				this.list_data.add(data);
 			}
 		}
 	}
 
+	/*
+	* Méthodes qui permets d'ajouter un noeuds à la DHT
+	* */
+
+	public void join(DHTNode node) {
+		this.addingNode = true; // on informe qu'on est entrain d'ajouter un noeuds.
+		//code pour ajouter un noeuds
+		System.out.println(node);
+		if(node.getID()<this.getID() ) {
+			if(node.getID()>this.leftNeighboor.getID()|| this.getID()< this.leftNeighboor.getID()) { // si le noeuds est inférieur au noeuds courant mais que le noeuds droit est supérieur entre le noeuds droit alors le noeuds ajouter ce situeras entre les deux
+				Logger_ajout_node.info("node "+ this.getID() +" a ajouté "+node.getID()+ " comme noeuds droits");
+				send(new Message("RIGHT",this), node.getMyNode());//setRightNeighboor(this);// au noeuds que l'on souhaite ajouter on set le voisin droit comme le noeuds courant
+				send(new Message("LEFT", leftNeighboor), node.getMyNode());//setLeftNeighboor(this.leftNeighboor);// au noeuds que l'on ajoute on set le voisin gauche comme notre voisin
+				send(new Message("RIGHT", node), leftNeighboor.getMyNode());//this.leftNeighboor.setRightNeighboor(node);
+				this.setLeftNeighboor(node);// on set le nouveau voisin droit comme le noeuds que l'on a ajouté
+			}
+			else {
+				send(new Message(node), leftNeighboor.getMyNode());//this.leftNeighboor.join(node);
+			}
+
+		}
+		else if(node.getID()>this.getID()){
+			if (node.getID()<this.rightNeighboor.getID() || this.getID() > this.rightNeighboor.getID()) { // si le noeuds à ajouter > noeuds courant mais <noeuds droit on ahoute entre les deux
+				Logger_ajout_node.info("node " + this.getID() +" a ajouté "+node.getID()+ " comme noeuds gauche");
+				send(new Message("LEFT", this), node.getMyNode());//node.setLeftNeighboor(this);
+				send(new Message("RIGHT", this.rightNeighboor), node.getMyNode());//node.setRightNeighboor(this.rightNeighboor);
+				send(new Message("LEFT", node), rightNeighboor.getMyNode());//this.rightNeighboor.setLeftNeighboor(node);
+				this.setRightNeighboor(node);
+			}
+			else {
+				send(new Message(node), rightNeighboor.getMyNode());//this.rightNeighboor.join(node);
+			}
+
+
+			this.addingNode = false;
+			this.treat_waiting_mess();
+		}
+	}
+
+	//  pour la fonction leave on triche, on ne passe pas par la couche réseau
+	public void leave() {
+		this.leftNeighboor.setRightNeighboor(this.rightNeighboor);
+		this.rightNeighboor.setRightNeighboor(this.leftNeighboor);
+	}
+	/**
+	 *Toutes les méthodes d'accesseurs au variable du noeuds
+	 */
+
+	private void get_data(Message msg) {} // fonction pour récupérer une donné, pas eu le temps de l'implémenter
+
     //retourne le noeud courant
-    private Node getMyNode() {return Network.get(this.id_transp);}
+    public Node getMyNode() {return Network.get(this.id_transp);}
 
     public int getID() {return this.ID;}
 
@@ -207,61 +312,16 @@ public class DHTNode implements EDProtocol{
     }
 
 
-	public void join(DHTNode node) {
-		System.out.println("JOIN    #################");
-			this.addingNode = true; // on informe qu'on est entrain d'ajouter un noeuds.
-			//code pour ajouter un noeuds
-			System.out.println(node);
-			if(node.getID()<this.getID() ) {
-				if(node.getID()>this.leftNeighboor.getID()|| this.getID()< this.leftNeighboor.getID()) { // si le noeuds est inférieur au noeuds courant mais que le noeuds droit est supérieur entre le noeuds droit alors le noeuds ajouter ce situeras entre les deux
-					Logger_ajout_node.info("node "+ this.getID() +" a ajouté "+node.getID()+ " comme noeuds droits");
-					send(new Message("RIGHT",this), node.getMyNode());//setRightNeighboor(this);// au noeuds que l'on souhaite ajouter on set le voisin droit comme le noeuds courant
-					send(new Message("LEFT", leftNeighboor), node.getMyNode());//setLeftNeighboor(this.leftNeighboor);// au noeuds que l'on ajoute on set le voisin gauche comme notre voisin
-					send(new Message("RIGHT", node), leftNeighboor.getMyNode());//this.leftNeighboor.setRightNeighboor(node);
-					this.setLeftNeighboor(node);// on set le nouveau voisin droit comme le noeuds que l'on a ajouté
-				}
-		 		else {
-					send(new Message(node), leftNeighboor.getMyNode());//this.leftNeighboor.join(node);
-				}
-
-			}
-			else if(node.getID()>this.getID()){
-				if (node.getID()<this.rightNeighboor.getID() || this.getID() > this.rightNeighboor.getID()) { // si le noeuds à ajouter > noeuds courant mais <noeuds droit on ahoute entre les deux
-					Logger_ajout_node.info("node " + this.getID() +" a ajouté "+node.getID()+ " comme noeuds gauche");
-					send(new Message("LEFT", this), node.getMyNode());//node.setLeftNeighboor(this);
-					send(new Message("RIGHT", this.rightNeighboor), node.getMyNode());//node.setRightNeighboor(this.rightNeighboor);
-					send(new Message("LEFT", node), rightNeighboor.getMyNode());//this.rightNeighboor.setLeftNeighboor(node);
-					this.setRightNeighboor(node);
-				}
-				else {
-					send(new Message(node), rightNeighboor.getMyNode());//this.rightNeighboor.join(node);
-				}
-
-
-			this.addingNode = false;
-			this.treat_waiting_mess();
-		}
-	}
-
-	// on ajoute une fonction appeler quand un noeuds désir quitter le réseau
-	public void leave() {
-		this.leftNeighboor.setRightNeighboor(this.rightNeighboor);
-		this.rightNeighboor.setRightNeighboor(this.leftNeighboor);
-	}
 	// lorsque le noeuds décide de quitter on envoi l'information au voisin qui arrête d'envoyer les message au noeuds, par la suite le noeuds donne à un de ces voisins les message reçu entre temps
 
 	public String show_neigh() {
 		return "noeuds courant " + this.getID()+" voisin gauche = "+ this.leftNeighboor.getID() + " voisin droit = "+ this.rightNeighboor.getID();
 	}
 
-	//on va représenter une donnée comme un message est un ID, l'ID sera désigné de façon aléatoire
-	// pour ajouter une data on peut se dire que
-	public void put_data(Data data){
-	// on va regardé l'ID du message et on routera si nécessaire
 	}
 
 
 
 
 
-}
+
--- PeersimForDHT/src/DHT/Initializer.java
+++ PeersimForDHT/src/DHT/Initializer.java
@@ -7,22 +7,7 @@ import java.util.Random;
 import java.util.ArrayList;
 import java.util.Comparator;
 
-/*
-  Module d'initialisation de helloWorld: 
-  Fonctionnement:
-    pour chaque noeud, le module fait le lien entre la couche transport et la couche applicative
-    ensuite, il fait envoyer au noeud 0 un message "Hello" a tous les autres noeuds
- */
-public class Initializer implements peersim.core.Control {
-    Random random = new Random();
-    private int tailleRandom = 100000;//1 000 000 000; //on définit la plage de valeur que va prendre ID
-    private int nextNode=1;//on définit la variable qui définit quelle est le prochain noeuds que l'on peut ajouter au réseau
-    private Node currentNode;
-    private DHTNode mynode;
-    
-    private ArrayList<DHTNode> DHT = new ArrayList<>();
-    public ArrayList<Integer> list_id = new ArrayList<>();
-	
+
     private int DHTPid;
 
     public Initializer(String prefix) {
@@ -30,6 +15,9 @@ public class Initializer implements peersim.core.Control {
     	this.DHTPid = Configuration.getPid(prefix + ".dhtProtocolPid");
     }
 
+	/*
+	Fonction pour ajouter un noeud à la DHT
+	 */
     public void add_a_node() {
     	int randNum = random.nextInt(1,this.tailleRandom);
     	int node_inter = random.nextInt(1,DHT.size());
@@ -65,8 +53,11 @@ public class Initializer implements peersim.core.Control {
 	}
 
 
-emitter = (DHTNode) Network.get(0).getProtocol(this.DHTPid);
-emitter.setTransportLayer(0, 0);
+	/*
+	On définit un noeuds emitter dont lequel on va se servir pour envoyer des messagges au noeuds de la DHT depuis le fichier Initializer
+	 */
+	emitter = (DHTNode) Network.get(0).getProtocol(this.DHTPid);
+	emitter.setTransportLayer(0, 0);
 	
 
 	
@@ -74,13 +65,12 @@ emitter.setTransportLayer(0, 0);
 	//définit les noeuds en liaisons au début de la simulation
 	for(int i =1; i<4; i++) {
 		int randNum = random.nextInt(1,tailleRandom);
-		nextNode = nextNode + 1;
-currentNode = Network.get(i);
+		nextNode = nextNode + 1; // variables qui permets de connaitre la place du nouveau noeuds que l'on peut ajouter à la DHT
+		currentNode = Network.get(i);
 		mynode = (DHTNode)currentNode.getProtocol(this.DHTPid);
 		mynode.setTransportLayer(i,randNum);
 		DHT.add(mynode);
 		list_id.add(randNum);
-		System.out.println("on initialise le noeud "+ i);
 	}
 	// on trie les noeuds et on attribue à chacun ses voisins
 	DHT.sort((Comparator<? super DHTNode>) new Comparator<DHTNode>() {
@@ -89,14 +79,16 @@ currentNode = Network.get(i);
 	        return (int)(node1.getID() - node2.getID());
 	    }
 	});
-	System.out.println("taille DHT = "+ DHT.size() + "network taille = "+ Network.size());
-	
+
+	/*
+	On mets en réseau les noeuds de la DHT
+	 */
 	DHT.get(0).setLeftNeighboor(DHT.get(DHT.size()-1));
 	DHT.get(0).setRightNeighboor(DHT.get(1));
 	DHT.get(0).setIdList(list_id);
 	System.out.println();
 
-	
+
 	DHT.get(DHT.size()-1).setRightNeighboor(DHT.get(0));
 	DHT.get(DHT.size()-1).setLeftNeighboor(DHT.get(DHT.size()-2));
 	DHT.get(DHT.size()-1).setIdList(list_id);
@@ -108,37 +100,27 @@ currentNode = Network.get(i);
 		DHT.get(i).setIdList(list_id);
 	System.out.println();
 	}
-/*	
-	for(int i=1 ; i<DHT.size();i++ ) {
-		emitter.send(helloMsg, Network.get(i));
+
+	/*
+	On interragit avec la DHT, on ajoute des noeuds, et on envoie des messages à des noeuds de la DHT qui effectuerons des actions en fonction
+
+	On ne peut pas ajouter plusieurs noeuds, puisque les actions dans l'initializer se passe de façons simultanné pour la DHT et entraîne des problèmes
+	et malheuresement nous n'avons pas trouvé le moyen d'ajouter des noeuds au sein de la simulation.
+	 */
+
+	this.add_a_node();
+	/*
+	for(int i = 0; i<10; i++) {
+		this.add_a_node();
 	}
-*/
+	*/
+	for(int i=0 ; i<DHT.size();i++ ) {
+		emitter.send(new Message(DHT.get(i).getID(), "hello"),DHT.get(i).getMyNode()); //envoie de message à tous les membres de la DHT
+	}
 	//methode pour ajouter un noeud
 
-	for(int i=0; i<DHT.size(); i++) {
-		System.out.println(DHT.get(i).show_neigh());
-	}
-	System.out.println("finis!!!!!!!!!!!!!");	
-	for(int i=0;i<14; i++) {
-	this.add_a_node();
-	try {Thread.sleep(1000);}
-	catch(Exception e) {System.out.println("fonctionne pas super");}
-	}
+	
 
-
-/*	for(int i=0; i<10;i++) {
-		this.add_a_node();
-		System.out.println("au noeuds "+ this.nextNode+"\n");
-	}*/
-
-	//pour chaque noeud, on fait le lien entre la couche applicative et la couche transport
-	//puis on fait envoyer au noeud 0 un message "Hello"
-//	for (int i = 1; i < nodeNb; i++) {
-//	    dest = Network.get(i);
-//	    current = (HelloWorld)dest.getProtocol(this.helloWorldPid);
-//	    current.setTransportLayer(i);
-//	    emitter.send(helloMsg, dest);
-//	}
 
 	System.out.println("Initialization completed");
 	return false;
